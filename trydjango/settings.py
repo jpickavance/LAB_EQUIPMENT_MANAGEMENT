90ef6e8e84e0259c2e770f81f80041512bfeae94
jpickavance <46566973+jpickavance@users.noreply.github.com>
2020-02-05 11:20:01 +0000

    added procfile

--- trydjango/settings.py
+++ trydjango/settings.py
@@ -20,8 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '=tb^m7e%2-mvp6_-!1k5!%ir+30_+s@=c^(famzc-ymqh^a_u2'
-#os.environ.get('DJANGO_SECRET_KEY')
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
